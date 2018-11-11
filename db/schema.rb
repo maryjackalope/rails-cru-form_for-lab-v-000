3fc42e35ebc0933b008f0b519329644147e15897
Mary Jack <maryjackinthebox@gmail.com>
2018-11-11 00:55:27 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181004235651) do
+
+  create_table "artists", force: :cascade do |t|
+  end
+
+  create_table "genres", force: :cascade do |t|
+  end
+
+  create_table "songs", force: :cascade do |t|
+  end
 
 end
